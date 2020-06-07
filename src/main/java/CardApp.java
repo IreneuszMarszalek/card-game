5e439b9febfd0f9bb85de4f2ba66ea5c9bed2a50
irek.m <irek.m@kurs.pl>
2020-06-07 22:49:07 +0200

    Class Card v1.0

--- src/main/java/CardApp.java
+++ src/main/java/CardApp.java
@@ -1,2 +1,19 @@
+import data.Card;
+
 public class CardApp {
+    public static void main(String[] args) {
+        Card card1 = new Card("Pik", "A");
+        System.out.println(card1.getColor());
+        System.out.println(card1.getValue());
+        System.out.println(card1.getColorSeniority());
+        System.out.println(card1.getValueSeniority());
+        card1.displayCard();
+
+        Card card2 = new Card("Kier", "J");
+        System.out.println(card2.getColor());
+        System.out.println(card2.getValue());
+        System.out.println(card2.getColorSeniority());
+        System.out.println(card2.getValueSeniority());
+        card2.displayCard();
+    }
 }
--- src/main/java/data/Card.java
+++ src/main/java/data/Card.java
@@ -21,23 +21,23 @@ public class Card {
     public Card(String color, String value) {
         boolean flag = true;
 
-        if(validateColor(color)){
-            this.color = color;
-        }else{
+        if(!validateColor(color)){
             System.out.println("Incorrect card color");
             flag = false;
         }
 
-        if (flag){
-            if(validateValue(value)){
-                this.value = value;
-            }else {
-                System.out.println("Incorrect card value");
-            }
+        if(!validateValue(value)){
+            System.out.println("Incorrect card value");
+            flag = false;
+        }
+
+        if(flag) {
+            this.color = color;
+            this.value = value;
         }
     }
 
-    private static boolean validateColor(String color){
+    private boolean validateColor(String color){
         if (color == null){
             return false;
         }
@@ -50,7 +50,7 @@ public class Card {
         return false;
     }
 
-    private static boolean validateValue(String value){
+    private boolean validateValue(String value){
         if (value == null){
             return false;
         }
@@ -61,5 +61,73 @@ public class Card {
             }
         }
         return false;
+    }
+
+    public byte getColorSeniority(){
+        if (this.color == null){
+            return -1;
+        }
+
+        byte index = 0;
+        if(validateColor(this.color)){
+            index = getIndexColorSeniority(this.color);
+            if (index != -1){
+                return cardColorSeniority[index];
+            }
+        }
+        return -1;
+    }
+
+    private byte getIndexColorSeniority(String color){
+        if (color == null){
+            return -1;
+        }
+        byte index = 0;
+
+        if(validateColor(color)){
+            for (String a:cardColorDesc) {
+                if (a.equals(color)){
+                    return index;
+                }
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    private byte getIndexValueSeniority(String value){
+        if (value == null){
+            return -1;
+        }
+        byte index = 0;
+
+        if(validateValue(value)){
+            for (String a:cardValueDesc) {
+                if (a.equals(value)){
+                    return index;
+                }
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    public byte getValueSeniority(){
+        if (this.value == null){
+            return -1;
+        }
+
+        byte index = 0;
+        if(validateValue(this.value)){
+            index = getIndexValueSeniority(this.value);
+            if (index != -1){
+                return cardValueSeniority[index];
+            }
+        }
+        return -1;
+    }
+
+    public void displayCard(){
+        System.out.println(this.getValue() + " " + this.getColor());
     }
 }
